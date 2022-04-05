23a0c8da383bda3a2c682d61c42025865b61c46c
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-04 22:14:26 -0500

    parallax design looking trippy af

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import img2 from './images/building2.png';
 import img3 from './images/building3.png';
 import img4 from './images/building4.png';
 import img5 from './images/clouds.png';
-import text from './images/text.png';
+import jna from './images/JNA-Design.png'
+import webart from './images/Webart.png'
+import scrollart from './images/Art-of-scroll.png'
+import Lottie from 'react-lottie-player'
+import orbit from './animations/orbit.json'
 
 
 function App() {
@@ -23,8 +27,8 @@ function App() {
   return (
     <div className="App">
       <div className='zoom' style={{backgroundColor: `hsl(${210 - offset}, 70%, 80%)`}}>
-      <img src={img5} alt="" id="img5" />
-      <img src={img1} alt="" id="img1"
+        <img src={img5} alt="" id="img5" />
+        <img src={img1} alt="" id="img1"
           style={{top: `-${offset * 0.1 + '%'}`, width: (100 + offset * 0.08) + '%'}}
         />
         <img src={img2} alt="" id="img2"
@@ -36,8 +40,14 @@ function App() {
         <img src={img4} alt="" id="img4"
           style={{left: `-${offset * 0.3 + '%'}`, height: (100 + offset * 0.3) + '%'}}
         />
-        <img src={text} alt="" id="text"
-          style={{top: `-${offset * 0.3 + '%'}`}}
+        <img src={webart} alt="" id="text1"
+          style={{top: `-${offset * 0.25 + '%'}`}}
+        />
+        <img src={jna} alt="" id="text"
+          style={{top: `-${offset * 0.8 + '%'}`}}
+        />
+        <img src={scrollart} alt="" id="text3"
+          style={{top: `-${offset * 0.09 + '%'}`}}
         />
         <div class="outer-circle"
           style={{bottom: `${20 + offset * 0.35 + '%'}`, width: (20 + offset * 0.15) + '%'}}
@@ -55,18 +65,28 @@ function App() {
       
 
       <div className='contentWrap'>
-        <div className='content'
-          style={{top: `-${10 + offset * 0.35 + '%'}`}}
-        >
-          <h2>Chicago Parallax Attempt</h2>
-          <p>Officia consectetur nulla cupidatat aliquip. Labore consectetur esse fugiat Lorem anim ea velit voluptate anim elit aliquip. Est est ipsum nulla occaecat fugiat sit consectetur laboris officia quis minim esse deserunt. Id aliqua reprehenderit sint duis et id occaecat cupidatat non magna. Irure ut laborum mollit ex in. Mollit amet amet ad esse nisi nostrud officia. Aliquip aute aute do anim labore irure officia eiusmod excepteur pariatur veniam esse proident.
-
-    Dolore est consequat culpa consectetur ex eu aute ex culpa deserunt minim duis et. Non culpa ea cillum amet reprehenderit laboris eu. Do officia do consequat cillum officia non ullamco irure. Sunt voluptate ullamco minim anim. Veniam eu sit nostrud consectetur occaecat aute sit ea aliqua veniam laboris pariatur. Dolore anim quis fugiat ut consectetur sint mollit sint nisi enim sit occaecat minim cillum. Irure duis ullamco consectetur sunt consequat magna excepteur magna quis ipsum deserunt reprehenderit.
-
-    Velit mollit et deserunt nostrud sit laborum. Ipsum Lorem est id eiusmod elit consectetur do nulla commodo est. Ipsum deserunt quis ut ea ad reprehenderit. Fugiat aute duis aliqua irure tempor Lorem non aliquip eiusmod. Velit commodo aliqua nisi cupidatat fugiat ad occaecat consectetur labore officia. Officia quis sit do ea in reprehenderit ea est magna. Do velit culpa nisi officia non quis ut amet Lorem nostrud aliqua ex nulla.
-
-    Amet culpa Lorem reprehenderit elit. Est excepteur laborum sit enim qui irure mollit nisi sint cillum ullamco cillum. Deserunt nulla reprehenderit sint labore eiusmod exercitation fugiat est. Reprehenderit et ut sunt nisi sunt.</p>
+        <div className='lottieWrap'>
+          <Lottie
+            loop
+            animationData={orbit}
+            play
+            style={{ width: 1920, height: 1080 }}
+          />
         </div>
+          <div className='textWrap'
+          style={{top: `-${offset * 0.09 + '%'}`}}
+          >
+            <div className='content' style={{backgroundColor: `hsla(${210 - offset}, 70%, 80%, .5)`}}>
+              <h2>Chicago Parallax Attempt</h2>
+              <p>Officia consectetur nulla cupidatat aliquip. Labore consectetur esse fugiat Lorem anim ea velit voluptate anim elit aliquip. Est est ipsum nulla occaecat fugiat sit consectetur laboris officia quis minim esse deserunt. Id aliqua reprehenderit sint duis et id occaecat cupidatat non magna. Irure ut laborum mollit ex in. Mollit amet amet ad esse nisi nostrud officia. Aliquip aute aute do anim labore irure officia eiusmod excepteur pariatur veniam esse proident.
+
+        Dolore est consequat culpa consectetur ex eu aute ex culpa deserunt minim duis et. Non culpa ea cillum amet reprehenderit laboris eu. Do officia do consequat cillum officia non ullamco irure. Sunt voluptate ullamco minim anim. Veniam eu sit nostrud consectetur occaecat aute sit ea aliqua veniam laboris pariatur. Dolore anim quis fugiat ut consectetur sint mollit sint nisi enim sit occaecat minim cillum. Irure duis ullamco consectetur sunt consequat magna excepteur magna quis ipsum deserunt reprehenderit.
+
+        Velit mollit et deserunt nostrud sit laborum. Ipsum Lorem est id eiusmod elit consectetur do nulla commodo est. Ipsum deserunt quis ut ea ad reprehenderit. Fugiat aute duis aliqua irure tempor Lorem non aliquip eiusmod. Velit commodo aliqua nisi cupidatat fugiat ad occaecat consectetur labore officia. Officia quis sit do ea in reprehenderit ea est magna. Do velit culpa nisi officia non quis ut amet Lorem nostrud aliqua ex nulla.
+
+        Amet culpa Lorem reprehenderit elit.</p>
+            </div>
+          </div>
         <div className='content2'></div>
       </div>
     </div>
